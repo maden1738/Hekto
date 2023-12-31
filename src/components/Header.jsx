8a3e47b0a3473f3ff798019f8c8aa4a9a334b5ea
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-12-31 19:45:05 +0545

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { CiMail } from "react-icons/ci";
 import { BsTelephone } from "react-icons/bs";
 export default function Header() {
   return (
-    <section className="flex  justify-between bg-primary py-2 font-body font-semibold text-white lg:px-[8%] xl:px-[15%]  ">
-      <div className="flex gap-9">
+    <section className="wrapper flex flex-col items-center gap-2 bg-primary py-2 font-body font-semibold text-white  lg:flex-row lg:justify-between lg:px-[8%] xl:px-[15%]  ">
+      <div className="flex gap-4 lg:gap-9">
         <div className="flex items-center gap-2">
           <CiMail className="text-xl" />
           <div>mhhasanul@gmail.com</div>
