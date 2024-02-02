2d14a5112835f1b4d73dc2acf2c1fea2c108661e
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-02-02 19:47:44 +0545

    review section

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -8,12 +8,14 @@ import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { addToCart, setCart } from "../app/slice/cartSlice";
 import useAuthenticate from "../hooks/useAuthenticate";
+import { FaStar } from "react-icons/fa";
 
 export default function SingleProduct() {
   const dispatch = useDispatch();
   const [productDetails, setProductDetails] = useState();
   let productId = useParams();
   const authenticate = useAuthenticate();
+  const access_token = localStorage.getItem("access_token");
 
   useEffect(() => {
     axios
@@ -37,49 +39,101 @@ export default function SingleProduct() {
       )}
 
       {productDetails && (
-        <div className="flex  items-center justify-center py-28 font-body shadow lg:gap-[41px]">
-          <div>
-            <img src={productDetails.image} alt="" className="max-w-[375px]" />
-          </div>
-          <div>
-            <p className="text-4xl font-semibold text-text">
-              {productDetails.name}
-            </p>
-            <p>${productDetails.price}</p>
-            <p className="text-subText">{productDetails.description}</p>
-            <div className="flex gap-[26px] py-4 text-text">
-              <button
-                className=""
-                type="button"
-                onClick={() => {
+        <div>
+          <section className=" flex flex-col items-center justify-center  gap-4  py-28 font-body lg:flex-row lg:gap-[41px]">
+            <div>
+              <img
+                src={productDetails.image}
+                alt=""
+                className="max-w-[375px] object-cover lg:h-[364px]"
+              />
+            </div>
+            <div className="flex flex-col ">
+              <p className="text-4xl font-semibold text-text">
+                {productDetails.name}
+              </p>
+              <p>${productDetails.price}</p>
+              <p className="text-subText">{productDetails.description}</p>
+              <div className="flex text-text lg:gap-[26px] lg:py-4">
+                <button
+                  className=""
+                  type="button"
+                  onClick={() => {
+                    authenticate(() => {
+                      dispatch(addToCart(productDetails));
+                      alert("added to cart");
+                    });
+                  }}
+                >
+                  Add to Cart
+                </button>
+                <FaRegHeart />
+              </div>
+              <div>
+                <p className="text-text">Categories:</p>
+                <p className="text-text">Tags:</p>
+                <p className="text-text">Share:</p>
+              </div>
+
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
                   authenticate(() => {
-                    dispatch(addToCart(productDetails));
-                    alert("added to cart");
+                    const review = {
+                      rating: event.target.rating.value,
+                      comment: event.target.comment.value,
+                    };
+                    axios
+                      .put(
+                        "https://ecommerce-sagartmg2.vercel.app/api/products/review/" +
+                          productId.slug,
+                        review,
+                        {
+                          headers: { Authorization: `Bearer ${access_token}` },
+                        },
+                      )
+                      .then((res) => toast("review added"))
+                      .catch((err) => console.log(err));
                   });
                 }}
+                className="mt-3 flex flex-col gap-2"
               >
-                Add to Cart
-              </button>
-              <FaRegHeart />
+                <select name="rating" className="border py-2">
+                  <option disabled selected>
+                    rating
+                  </option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <textarea
+                  type="text"
+                  name="comment"
+                  className=" border px-2 py-2"
+                  placeholder="comment"
+                />
+                <button className="btn-red">Add Review</button>
+              </form>
             </div>
-            <div>
-              <p className="text-text">Categories:</p>
-              <p className="text-text">Tags:</p>
-              <p className="text-text">Share:</p>
-            </div>
-          </div>
+          </section>
+          <section className="wrapper ">
+            <p className="text-2xl text-subText">Reviews:</p>
+            {productDetails.reviews.map((review) => (
+              <>
+                <div className="mb-10 flex items-center justify-start gap-4 text-text">
+                  <p className="flex items-center gap-2">
+                    {review.rating}
+                    <FaStar className="text-yellow-400" />
+                  </p>
+                  <p>{review.comment}</p>
+                </div>
+              </>
+            ))}
+          </section>
         </div>
       )}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          toast.error("login requried.");
-        }}
-      >
-        <input type="text" className="border px-4 py-2" />
-        <input type="text" className="border px-4 py-2" />
-        <button className="btn-red">Add Review</button>
-      </form>
     </>
   );
 }
